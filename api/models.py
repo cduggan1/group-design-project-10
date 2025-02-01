b6b7a2e824f7e609cf4dc7aa5ec74a85961186f8
max-romagnoli <massimiliano.romagnoli02@gmail.com>
2025-02-01 16:57:39 +0000

    Add react app and gis in django

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.gis.db import models as gis_models
 
-# Create your models here.
+
+class TestGeometry(models.Model):
+    point = gis_models.PointField()
+
+    class Meta:
+        verbose_name_plural = 'Test Geometries'
+        verbose_name = 'Test Geometry' 
--- weather/settings.py
+++ weather/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django.contrib.gis',
-    # 'leaflet',
+    'django.contrib.gis',
+    'leaflet',
 ]
 
 MIDDLEWARE = [
