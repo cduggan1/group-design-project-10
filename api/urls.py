9d3ed69ec78b38bc0f3a1f0b84c27d8a9d8afb16
max-romagnoli <massimiliano.romagnoli02@gmail.com>
2025-02-19 17:55:26 +0000

    Add view returing top 5 trail near given location

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import get_weather, get_address, get_directions, get_all_trails
+from .views import *
 
 urlpatterns = [
     path('weather/', get_weather),
     path('address/', get_address),
     path('directions/', get_directions),
-    path('activities/trails/all', get_all_trails)
+    path('activities/trails/all', get_all_trails),
+    path('activities/trails/top/', get_top_trails_near_location)
 ]
